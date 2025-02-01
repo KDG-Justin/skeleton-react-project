fda9e0fd64752436ef99c2f2d7a5b04ce3a15146
KDG-Justin <justin.d.jdb@gmail.com>
2025-02-01 03:18:47 +0100

    project setup

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # skeleton-react-project
+
+A skeleton React project to help start your React project
+It contains: 
+- Typescript
+- NPM
+- VITE
+- PRETTIER
+- ESTLINT
+- BOOTSTRAP 5
---
+++ skeleton/src/App.tsx
@@ -0,0 +1,16 @@
+import axios from 'axios'
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+axios.defaults.baseURL = import.meta.env.VITE_BACKEND_URL
+
+
+
+function App() {
+
+    return (
+            <div>hi</div>
+    )
+}
+
+export default App
